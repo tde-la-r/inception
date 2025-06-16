cd4a83bd5eabb8ee77620916028026a9b2c41437
tde-la-r <theaudelaruelle@yahoo.fr>
2025-06-16 20:10:39 +0200

    january

--- srcs/requirements/mariadb/tools/install_mysql.sh
+++ srcs/requirements/mariadb/tools/install_mysql.sh
@@ -1,10 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
-service mariadb start;
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#MARIADB_USER=tdelar
+#MARIADB_PASSWORD=GFJ54DGHFGD422S
+#MARIADB_DATABASE=tdelar_db
+
+#mariadb-install-db
+
+# Start the service
+service mariadb start
+
+#sleep 5
+
+# Create the database named $MARIADB_DATABASE if it does not exist
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+
+# Create a new Mariadb user with it's Password
+mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+
+# Grant all privilage to the user on the specified database
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' ;"
+
+# Reload the privilege to assur the user now have the permissions
 mariadb -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+
+# Shutdown the server
+service mariadb stop
+
+# Restart the server
+mariadbd-safe
+#mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking=0 --bind-address=0.0.0.0 &
--- srcs/requirements/wordpress/tools/install_wp.sh
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -2,7 +2,7 @@
 
 echo "Waiting for MariaDB to be available..."
 for i in {1..60}; do
-    if mysqladmin ping -h "${WORDPRESS_DB_HOST}" -u "${MARIADB_USER}" "--password=${MARIADB_PASSWORD}" --silent; then
+    if mysqladmin ping -h "mariadb" -u "${MARIADB_USER}" "--password=${MARIADB_PASSWORD}" --silent; then
         echo "MariaDB is up."
         break
     else
@@ -16,17 +16,16 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download
 wp config create    --allow-root \
-					--dbname=$SQL_DATABASE \
-                    --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD \
+					--dbname=$MARIADB_DATABASE \
+                    --dbuser=$MARIADB_USER \
+                    --dbpass=$MARIADB_PASSWORD \
                     --dbhost=mariadb:3306 --path='/var/www/wordpress'
-wp core install --url="bvasseur.42.fr" \
+wp core install --url="tde-la-r.42.fr" \
                 --title="BVASSEUR AKA LE MEILLEUR MJ" \
-                --admin_user=${WORDPRESS_ADMIN_USR} \
-                --admin_password=${WORDPRESS_ADMIN_PWD} \
-                --admin_email=${WORDPRESS_ADMIN_MAIL} \
+                --admin_user=${WP_ADMIN_USER} \
+                --admin_password=${WP_ADMIN_PWD} \
+                --admin_email=${WP_ADMIN_MAIL} \
                 --allow-root
-wp user create ${WORDPRESS_USR} ${WORDPRESS_MAIL} --role=author --user_pass=${WORDPRESS_PWD} --allow-root
+wp user create ${WP_USER} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PWD} --allow-root
 chmod -R 775 wp-content
 exec php-fpm7.4 -F
-
